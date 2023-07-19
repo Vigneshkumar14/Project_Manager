6d1085883678163a5fe004fecc833b82773eac0d
Vigneshkumar14 <vigneshkumargu@gmail.com>
2023-07-19 18:59:26 +0530

    Fixed Cookie issue

--- Server/utils/cookieOptions.js
+++ Server/utils/cookieOptions.js
@@ -1,9 +1,9 @@
-import config from "../config/index.js";
 const cookieOptions = {
   expires: new Date(Date.now() + 2 * 24 * 3600000), // 2 days
   httpOnly: true,
   sameSite: "none",
   secure: true,
+  domain: process.env.DOMAIN,
 };
 
 export default cookieOptions;
